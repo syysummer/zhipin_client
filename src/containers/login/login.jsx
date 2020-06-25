4a63383b7450ea298926f91247cb6cf37950ac23
sunyangyang <syysummer@yeah.net>
2020-06-25 20:32:24 +0800

    11

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -41,7 +41,7 @@ class Login extends Component {
     }
     return (
       <div>
-          <NavBar type="primary">Y&nbsp;Y&nbsp;直&nbsp;聘</NavBar>
+          <NavBar type="primary">Y&nbsp;Y&nbsp;直&nbsp;聘login页面</NavBar>
            <Logo/>
            <WingBlank>
            {msg ? <p className='error-msg'>{msg}</p> : null}
